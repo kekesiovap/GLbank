82db03dfca5f995bfba5ac7232b33fbc4448194a
kekesiovap <43435759+kekesiovap@users.noreply.github.com>
2019-04-11 12:35:41 +0200

    Add files via upload

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,7 +2,12 @@ package sample;
 
 import database.Database;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import org.w3c.dom.Node;
+
 import java.sql.SQLException;
 
 public class Controller {
@@ -17,6 +22,26 @@ public class Controller {
         Employee emp = database.getEmployee(login,password);
         if(emp != null){
             System.out.println("ID: "+emp.getId()+" | First name: "+emp.getFname()+" | Last name: "+emp.getLname()+" | Position ID: "+emp.getPosition());
+        }
+        try{
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, login);
+            preparedStatement.setString(2, password);
+            resultSet = preparedStatement.executeQuery();
+            if(!resultSet.next()){
+                infoBox("Incorrect input", null, "Failed");
+            }else{
+                infoBox("Login Successfull",null,"Success");
+                Node node = (Node)event.getSource();
+                dialogStage = (Stage) node.getScene().getWindow();
+                dialogStage.close();
+                scene = new Scene(FXMLLoader.load(getClass().getResource("sample1.fxml")));
+                dialogStage.setScene(scene);
+                dialogStage.show();
+            }
+        }
+        catch(Exception e){
+            e.printStackTrace();
         }
     }
 }
